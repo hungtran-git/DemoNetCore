d70c61a8e1e5a219c1f84c6a589fd020d5bf7360
Hung Tran <hungtran9900@live.com>
2021-04-01 13:50:46 +0700

    First commit

---
+++ jwt-authentication-api-example/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using jwt_authentication_api_example.Helpers;
+using jwt_authentication_api_example.Models;
+using jwt_authentication_api_example.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jwt_authentication_api_example.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+    }
+}
---
+++ jwt-authentication-api-example/Helpers/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jwt_authentication_api_example.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
